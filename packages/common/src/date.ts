8d549dd7765ca11d9fc31da8b5d18599af1ed003
Wu Huijie <hjwu85@gmail.com>
2020-03-26 18:19:08 +0800

    add new date util funcs

--- packages/common/src/date.ts
+++ packages/common/src/date.ts
@@ -1,9 +1,13 @@
 import { convertToInt } from './numbers';
 
-export function convertToDate(dateStr: string): Date | null {
+export function isDefined(dateStr: string): boolean {
+  return !!dateStr && dateStr.includes('-');
+}
+
+export function convertToDate(dateStr: string): Date {
   if (dateStr && dateStr.includes('-')) {
     const parts = dateStr.split('-');
     return new Date(convertToInt(parts[0]), convertToInt(parts[1]) - 1);
   }
-  return null;
+  return new Date();
 }
