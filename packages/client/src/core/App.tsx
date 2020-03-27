3ae435542755f814b6283d07652b70ed028f11c6
Wu Huijie <hjwu85@gmail.com>
2020-03-27 16:47:26 +0800

    fixed routing for remote server

--- packages/client/src/core/App.tsx
+++ packages/client/src/core/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, FunctionComponent, lazy } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createAppStore } from './store';
 import { appInitialState } from '../reducers';
